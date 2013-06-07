ea10ea5dd5cd5c7824889114f036354fbb3c3c38
Maiah Macariola <jjmmacariola@gmail.com>
2013-06-07 14:20:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 function http_fiddle() {
     var http_f = {};
-    http_f.createServer = function(handler) {
+    var hn = null;
+
+    var server = {};
+    server.listen = function(port) {
+        var resMsg = '';
+        var res = {};
+        res.writeHead = function(statusCode, opts) {};
+        res.write = function(msg) {
+            resMsg += msg;
+        };
+        res.end = function(msg) {
+            resMsg += msg;
+            console.log(resMsg);
+        };
+
         setTimeout(function() {
-            handler({}, {});
-        }, 2000);
+            hn({}, res);
+        }, 3000);
     };
+
+    http_f.createServer = function(handler) {
+        hn = handler;
+        return server;
+    };
+
     return http_f;
 }
