998e6675fc17f9341caa41ddd2464e9a4ea6db24
Maiah Macariola <jjmmacariola@gmail.com>
2013-06-07 13:40:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function http_fiddle() {
     var http_f = {};
-    http_f.createServer = function(path, handler) {
+    http_f.createServer = function(handler) {
         setTimeout(function() {
             handler({}, {});
         }, 2000);
